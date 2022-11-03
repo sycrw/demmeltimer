68aed58680565bda2b077b27a308b08ac87d90dd
sycrw <timmartinkausemann@gmail.com>
2022-11-03 17:49:41 +0100

    some erros fixed

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 checkCookies();
 function checkCookies(){
     var cookie = getCookie("pass");
-    console.log(cookie);
     if(cookie!="test"){
         window.location = "verification.html";
     }
 
 }
-function setCookie(cname, cvalue, exdays) {
-    
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+
 
 function getCookie(cname) {
     let name = cname + "=";
@@ -29,12 +22,4 @@ function getCookie(cname) {
         }
     }
     return "";
-}
-function login(){
-    pas = document.getElementById("pas").value;
-    console.log(pas);
-    if(pas== "test"){
-        window.location ="index.html";
-    }
-    setCookie("pass",pas);
 }
