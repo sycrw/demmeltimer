a8a69a15dd9ded9d29ed69d9a3b940518a6bca79
sycrw <timmartinkausemann@gmail.com>
2022-11-03 17:49:45 +0100

    lohuiz fgek

---
+++ verification.js
@@ -0,0 +1,15 @@
+function login(){
+    pas = document.getElementById("pas").value;
+    console.log(pas);
+    if(pas== "test"){
+        window.location ="index.html";
+    }
+    setCookie("pass",pas,1);
+} 
+function setCookie(cname, cvalue, exdays) {
+    
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
